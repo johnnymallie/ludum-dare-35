f5ab664596c45874afb01995471fc7b912e504a9
johnnymallie <johnny.mallie@webadev.com>
2016-04-16 16:49:51 +0200

    ça commence ...

--- LudumDare35/LudumDare35/src/Level/Level1.ts
+++ LudumDare35/LudumDare35/src/Level/Level1.ts
@@ -8,10 +8,11 @@
         platform;
         map: Phaser.Tilemap;
         layer: Phaser.TilemapLayer;
+        layerWall = Phaser.TilemapLayer;
         
         preload() {
             console.log('tilemap');
-            this.load.tilemap('map', 'assets/images/levels/level1/map1.json', null, Phaser.Tilemap.TILED_JSON);
+            this.load.tilemap('map', 'assets/images/levels/level1/map2.json', null, Phaser.Tilemap.TILED_JSON);
             this.load.image('tiles', 'assets/images/levels/level1/tileset.png');
             this.load.image('playerTriangle', 'assets/images/elements/playerTriangle.png');
             this.load.image('playerCircle', 'assets/images/elements/playerCircle.png');
@@ -22,26 +23,28 @@
             //this.background = this.add.sprite(0, 0, 'level1');
 
             //Physics doesn't work
-            //this.game.physics.startSystem(Phaser.Physics.ARCADE);
+            this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
             /*this.music = this.add.audio('music', 1, false);
             this.music.play();
             */
-           
+            
            
             this.map = this.add.tilemap('map');
             console.log(this.map.height);
             this.map.addTilesetImage('tileset','tiles'); 
             
             this.layer = this.map.createLayer('render');
+
+            this.layer = this.map.createLayer('wall');
             console.log(this.map);
             
             this.layer.resizeWorld();
             //this.layer.fixedToCamera = false;
 
             this.game.camera.y = 2560;
-            console.log(this.layer);    
-            //this.map.setCollisionBetween(1, 100, true, 'Collision');
+             
+            this.map.setCollisionBetween(1, 10000, true,'wall');
 
             this.player = new Player(this.game, Game.global.playerX, Game.global.playerY);
             
@@ -52,9 +55,14 @@
         }
 
         update() {
-            this.game.camera.y -= 4;
-            this.player.y -= 4;
-            console.log(this.game.camera.y);
+            this.game.camera.y -= 1;
+            this.player.y -= 1;
+            //this.game.physics.arcade.collide(this.player, this.layer,this.test, null, this);
+            this.game.physics.arcade.overlap(this.player, this.layer, this.test,null,this)
+        }
+
+        test(player, layer) {
+            console.log('collision');
         }
 
     }
