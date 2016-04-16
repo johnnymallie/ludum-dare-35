150350066f86ab49b13f75f1175c860f727e7e71
Pythorogus <pierre.gomba@gmail.com>
2016-04-16 19:16:41 +0200

    main menu

--- LudumDare35/LudumDare35/src/Loader/Preloader.ts
+++ LudumDare35/LudumDare35/src/Loader/Preloader.ts
@@ -9,8 +9,8 @@
             //this.preloadBar = this.add.sprite(200, 250, 'preloadBar');
            // this.load.setPreloadSprite(this.preloadBar);
             //  Load our actual games assets
-            //this.load.image('titlepage', 'assets/titlepage.jpg');
-            //this.load.image('logo', 'assets/logo.png');
+            this.load.image('backgroundMenu', 'assets/images/menus/backgroundMenu.png');
+            this.load.image('logo', 'assets/images/menus/logo.png');
             //this.load.audio('music', 'assets/title.mp3', true);
             //this.load.spritesheet('simon', 'assets/simon.png', 58, 96, 5);
             //this.load.spritesheet('knight', 'assets/knight2.png', 48, 96, 3);
@@ -19,15 +19,14 @@
         }
 
         create() {
-
             //var tween = this.add.tween(this.preloadBar).to({ alpha: 0 }, 1000, Phaser.Easing.Linear.None, true);
             // tween.onComplete.add(this.startMainMenu, this);
             this.startMainMenu();
         }
 
         startMainMenu() {
-            //this.game.state.start('MainMenu', true, false);
-            this.game.state.start('Level1', true, false);
+            this.game.state.start('MainMenu', true, false);
+            //this.game.state.start('Level1', true, false);
 
         }
 
