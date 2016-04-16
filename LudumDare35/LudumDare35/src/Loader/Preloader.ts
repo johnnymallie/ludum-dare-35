60f335e5a676a8d7dd5a3fa23915d6007d370f96
Pythorogus <pierre.gomba@gmail.com>
2016-04-16 09:40:44 +0200

    Structure proposition

---
+++ LudumDare35/LudumDare35/src/Loader/Preloader.ts
@@ -0,0 +1,36 @@
+﻿module Castlevania {
+
+    export class Preloader extends Phaser.State {
+
+        preloadBar: Phaser.Sprite;
+
+        preload() {
+            //  Set-up our preloader sprite
+            this.preloadBar = this.add.sprite(200, 250, 'preloadBar');
+            this.load.setPreloadSprite(this.preloadBar);
+            //  Load our actual games assets
+            this.load.image('titlepage', 'assets/titlepage.jpg');
+            this.load.image('logo', 'assets/logo.png');
+            this.load.audio('music', 'assets/title.mp3', true);
+            //this.load.spritesheet('simon', 'assets/simon.png', 58, 96, 5);
+            this.load.spritesheet('knight', 'assets/knight2.png', 48, 96, 3);
+            this.load.image('level1', 'assets/level1.png');
+            this.load.image('platform', 'assets/platform.png');
+        }
+
+        create() {
+
+            var tween = this.add.tween(this.preloadBar).to({ alpha: 0 }, 1000, Phaser.Easing.Linear.None, true);
+            tween.onComplete.add(this.startMainMenu, this);
+
+        }
+
+        startMainMenu() {
+            //this.game.state.start('MainMenu', true, false);
+            this.game.state.start('Level1', true, false);
+
+        }
+
+    }
+
+}
