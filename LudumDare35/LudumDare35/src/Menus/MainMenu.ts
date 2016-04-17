e1eceb7a42cd4583f911cfe784205ff8f983c61d
Pythorogus <pierre.gomba@gmail.com>
2016-04-17 19:12:27 +0200

    Commit de menus

--- LudumDare35/LudumDare35/src/Menus/MainMenu.ts
+++ LudumDare35/LudumDare35/src/Menus/MainMenu.ts
@@ -11,23 +11,26 @@
             this.background = this.add.sprite(0, 0, 'backgroundMenu');
             this.background.alpha = 0;
 
-            this.logo = this.add.sprite(this.world.centerX, -300, 'logo');
+            this.logo = this.add.sprite(this.world.centerX, this.world.centerY, 'logo');
+            this.logo.alpha = 0;
             this.logo.anchor.setTo(0.5, 0.5);
-
-            this.add.tween(this.background).to({ alpha: 1 }, 2000, Phaser.Easing.Bounce.InOut, true);
-            this.add.tween(this.logo).to({ y: 220 }, 2000, Phaser.Easing.Elastic.Out, true, 2000);
-
+            
+            this.add.tween(this.background).to({ alpha: 1 }, 2000, Phaser.Easing.Linear.None, true);
+            this.add.tween(this.logo).to({ alpha: 1 }, 2000, Phaser.Easing.Linear.None, true);
+            
             this.input.onDown.addOnce(this.fadeOut, this);
-
         }
 
         fadeOut() {
             this.startSound.play();
+            /*
             this.add.tween(this.background).to({ alpha: 0 }, 2000, Phaser.Easing.Linear.None, true);
             var tween = this.add.tween(this.logo).to({ y: 800 }, 2000, Phaser.Easing.Linear.None, true);
-
+            */
+            this.add.tween(this.background).to({ alpha: 0 }, 2000, Phaser.Easing.Linear.None, true);
+            var tween = this.add.tween(this.logo).to({ alpha: 0 }, 2000, Phaser.Easing.Linear.None, true);
+        
             tween.onComplete.add(this.startGame, this);
-
         }
 
         startGame() {
